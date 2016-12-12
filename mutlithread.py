5b4b14043541b90f3bf9be8c4ea2c2977c72127c
Moritz Kornmeier <moritz_johannes.kornmeier@student.reutlingen-university.de>
2016-12-12 22:48:50 +0100

    multithreading #2

--- mutlithread.py
+++ mutlithread.py
@@ -1,32 +1,39 @@
 import threading
-import timeit
 from queue import Queue
 import pafy
 import time
 
-start = timeit.timeit()
+lock = threading.Lock()
+
+def printAudioDetail(item):
+    p = item
+    best = p.getbestaudio()
+    print(p.title)
+    print(threading.current_thread().name, best.url)
 
 def worker():
     while True:
-        p = video['pafy']
-        best = p.getbestaudio()
-        print(p.title)
-        print(threading.current_thread().name, best.url)
+        time.sleep(1)
+        item = q.get()
+        printAudioDetail(item)
+        q.task_done()
+
 
 playlist = pafy.get_playlist("PLpaD0ybYH0S3XOMnC8ADVycFyEcjRJ6Aj")
 videos = playlist['items']
 
-#fill Queue
-q = Queue()
-
-
-for video in videos:
+for i in range(100):
   t = threading.Thread(target=worker)
   t.daemon = True
   t.start()
 
-time.sleep(10)
+#fill Queue
+q = Queue()
+for video in videos:
+    q.put(video['pafy'])
 
-end = timeit.timeit()
-print (end - start)
+q.join()
+
+time.sleep(5)
+
 
