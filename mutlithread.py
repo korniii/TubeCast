1ff551e65d88e572ac04ef68d9ec640c79cf5d89
Moritz Kornmeier <moritz_johannes.kornmeier@student.reutlingen-university.de>
2016-12-12 22:54:43 +0100

    multithreading #3

--- mutlithread.py
+++ mutlithread.py
@@ -3,17 +3,20 @@ from queue import Queue
 import pafy
 import time
 
+# lock to serialize console output
 lock = threading.Lock()
 
+#prints all Title and AudioLink of Profile
 def printAudioDetail(item):
     p = item
     best = p.getbestaudio()
     print(p.title)
     print(threading.current_thread().name, best.url)
 
+#worker method, thread method
 def worker():
+    time.sleep(1)
     while True:
-        time.sleep(1)
         item = q.get()
         printAudioDetail(item)
         q.task_done()
@@ -22,18 +25,20 @@ def worker():
 playlist = pafy.get_playlist("PLpaD0ybYH0S3XOMnC8ADVycFyEcjRJ6Aj")
 videos = playlist['items']
 
+#Threadpool initialization, number of Threads
 for i in range(100):
   t = threading.Thread(target=worker)
   t.daemon = True
   t.start()
 
-#fill Queue
+#fill Queue, gets processed by the Threads
 q = Queue()
 for video in videos:
     q.put(video['pafy'])
 
 q.join()
 
-time.sleep(5)
+#if method stops before threads --> Exception
+time.sleep(10)
 
 
